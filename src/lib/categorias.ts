63f056a56797e7dd1fc56e67f75c7b1a8a4c2cab
Aitor <aitor@taurix.es>
2026-07-27 20:51:32 +0200

    ads

---
+++ src/lib/categorias.ts
@@ -0,0 +1,9 @@
+export const categorias = [
+  { nombre: 'Actuadores', slug: 'actuadores' },
+  { nombre: 'Paneles Táctiles KNX', slug: 'paneles-tactiles-knx' },
+  { nombre: 'Fuentes de alimentación', slug: 'fuentes-de-alimentacion' },
+  { nombre: 'Gateways', slug: 'gateways' },
+  { nombre: 'Interfaces y acopladores', slug: 'interfaces-y-acopladores' },
+  { nombre: 'Sensores', slug: 'sensores' },
+  { nombre: 'Servidores', slug: 'servidores' },
+];
